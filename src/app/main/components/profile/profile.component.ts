378a9065a1c806b4dbd882fd530544cced261889
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-10-15 21:51:59 +0800

    change password

--- src/app/main/components/profile/profile.component.ts
+++ src/app/main/components/profile/profile.component.ts
@@ -19,6 +19,7 @@ export class ProfileComponent {
   file: any = null
 
   formDetails: FormGroup
+  passwordDetails: FormGroup
 
   //suffixes
   titles: string[] = ['Sr', 'Jr', 'II', 'III', 'IV', 'V'];
@@ -71,6 +72,12 @@ export class ProfileComponent {
       fax_number: [null, [Validators.pattern('(09)[0-9]{9}')]],
       email: [null, [Validators.required, Validators.email]],
       website: [null, [Validators.maxLength(128)]],
+    })
+
+    this.passwordDetails = this.fb.group({
+      current_password: [null, [Validators.required]],
+      new_password: [null, [Validators.required, Validators.minLength(8)]],
+      password_confirmation: [null, [Validators.required, Validators.minLength(8)]]
     })
   }
 
@@ -197,6 +204,46 @@ export class ProfileComponent {
   getLocation() {
     // this.municipalities = this.ls.getMunicipality()
     // this.regions = this.ls.getRegions()
+  }
+
+  changePassword() {
+    if(this.passwordDetails.invalid) {
+      const firstInvalidControl: HTMLElement = document.querySelector('form .ng-invalid')!;
+      
+      if (firstInvalidControl) {
+        firstInvalidControl.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+  
+      this.passwordDetails.markAllAsTouched();
+      return;
+    }
+
+    const new_password = this.passwordDetails.get('new_password')?.value
+    const confirm_password = this.passwordDetails.get('password_confirmation')?.value
+
+    if(new_password != confirm_password) {
+      this.gs.errorAlert("Password Doesn't Match!", "Please double check you inputs.")
+      return
+    }
+
+    console.log(this.passwordDetails.value)
+    this.ds.post('supervisor/profile/change-password', '', this.passwordDetails.value).subscribe(
+      response => {
+        this.gs.successAlert(response.title, response.message)
+        this.passwordDetails.reset(this.passwordDetails.value)
+      },
+      error => {
+        if(error.status === 422) {
+          this.gs.errorAlert("Invalid Input!", "Please double check you inputs.")
+        }
+        else if(error.status === 403) {
+          this.gs.errorAlert(error.error.title, error.error.message)
+        }
+
+        console.error(error)
+
+      }
+    )
   }
   
 }
