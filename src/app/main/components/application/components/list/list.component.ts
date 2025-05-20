e67ab631029ea8d87015bc2bdfca005ed4d9d118
LazyPotatoGamer <krystacoronel13@gmail.com>
2025-05-20 20:38:40 +0800

    removing of accepted students in the application tab

--- src/app/main/components/application/components/list/list.component.ts
+++ src/app/main/components/application/components/list/list.component.ts
@@ -21,32 +21,32 @@ export class ListComponent {
 
   unfilteredStudents: any
   dataSource: any = new MatTableDataSource<any>();
-  
+
   classList: any = []
   statusFilter: any = ''
   // classFilter: string = 'all'
 
   isLoading: boolean = false
-  
+
   @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
 
   constructor(
-    private paginatorIntl: MatPaginatorIntl, 
+    private paginatorIntl: MatPaginatorIntl,
     private changeDetectorRef: ChangeDetectorRef,
     private router: Router,
     private ds: DataService,
     private us: UserService
   ) {
     this.paginator = new MatPaginator(this.paginatorIntl, this.changeDetectorRef);
-    
+
     const nameFilterPredicate = (data: any, search: string): boolean => {
       return data.full_name.toLowerCase().includes(search);
-    } 
-    
+    }
+
     const emailFilterPredicate = (data: any, search: string): boolean => {
       // return data.student_profile.student_number.toLowerCase().includes(search);
       return data.email.toLowerCase().includes(search);
-    } 
+    }
 
     const filterPredicate = (data: any, search: string): boolean => {
       return (
@@ -70,22 +70,22 @@ export class ListComponent {
         console.log(students)
         let studentsList = students.map((student: any) => {
 
-          let status_label 
+          let status_label
 
-          if (student.status == 3) 
+          if (student.status == 3)
             status_label = 'For Approval';
-          else if (student.status == 4) 
+          else if (student.status == 4)
             status_label = 'Not Approved';
-          else if (student.status == 5) 
+          else if (student.status == 5)
             status_label = 'For Schedule';
-          else if (student.status == 6) 
+          else if (student.status == 6)
             status_label = 'Reschedule';
-          else if (student.status == 7) 
+          else if (student.status == 7)
             status_label = 'For Interview';
-          else if (student.status == 8) 
-            status_label = 'Accepted';
-          
-          student.user.email = student.user.email + '@gordoncollege.edu.ph' 
+          // else if (student.status == 8)
+          //   status_label = 'Accepted';
+
+          student.user.email = student.user.email + '@gordoncollege.edu.ph'
 
           return {
             full_name: student.user.first_name + " " + student.user.last_name,
@@ -94,9 +94,9 @@ export class ListComponent {
             label: status_label,
             ...student.user,
             created_at: student.created_at,
-            id: student.id, 
-          } 
-        })  
+            id: student.id,
+          }
+        })
         console.log(studentsList)
 
         this.unfilteredStudents = studentsList
@@ -121,12 +121,12 @@ export class ListComponent {
   applyFilter() {
     //class filter
     let students = this.unfilteredStudents
-    
+
     if(this.statusFilter) {
       students = students.filter((student: any) => {
         return student.status == this.statusFilter
       })
-    } 
+    }
 
     this.dataSource.data = students
   }
